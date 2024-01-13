9ab607d6bc6c39129c169486edc7bac025e6392e
Sahil <garg07825@gmail.com>
2024-01-13 10:41:09 +0530

    in index file iset up routes for handle te request

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import { config } from "dotenv";
 import bodyParser from "body-parser";
 import { errorMiddleware } from "./middleware/errorMiddleware.js";
+import ProductRoute from "./routes/ProductRoute.js";
 
 // Initialize Express application
 export const app = express();
@@ -14,6 +15,9 @@ config({ path: "./.env" });
 app.use(bodyParser.json()); // Parse incoming JSON requests
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(express.json()); // Parse JSON payloads
+
+// Set up routes for different functionalities
+app.use("/", ProductRoute); // User-related routes
 
 // Use error handling middleware to manage and respond to errors
 app.use(errorMiddleware);
