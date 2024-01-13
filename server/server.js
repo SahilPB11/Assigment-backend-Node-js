ae6bf406b6e5d3c8abf4ef8373a7a0ded29f51dc
Sahil <garg07825@gmail.com>
2024-01-13 10:21:19 +0530

    project connect with mongoDB

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 // Import the database connection function and the configured Express application
-
+import connectDb from "./config/connectDb.js";
 import { app } from "./index.js";
 
 // Retrieve the port number from environment variables
 const port = process.env.PORT;
 
-// start the server on the specified port
+// Connect to the database and start the server on the specified port
+connectDb().then(
   app.listen(port, () => console.log(`Server is Working on ${port}`))
+);
